3695ef7f598e203d5762f7707c5fbfa4a91e6fec
marco.sanman <marco.sanman@ED09012096P395.educa.jcyl.es>
2025-04-30 11:35:32 +0200

    VistaListado con el ejercicio 2y3

--- src/jcolonia/daw2024/e3a/VistaListado.java
+++ src/jcolonia/daw2024/e3a/VistaListado.java
@@ -18,6 +18,22 @@ public class VistaListado extends VistaGeneral {
 	 * propio texto de la línea.
 	 */
 	private static final String FORMATO_LISTADO = "  %d: %s%n";
+	/**
+	 * Número de entradas, importaciones y exportaciones y borrados
+	 */
+	private int numEntradas;
+	/**
+	 * Número de entradas, importaciones y exportaciones y borrados
+	 */
+	private int numImportaciones;
+	/**
+	 * Número de entradas, importaciones y exportaciones y borrados
+	 */
+	private int numExportaciones;
+	/**
+	 * Número de entradas, importaciones y exportaciones y borrados
+	 */
+	private int numBorrados;
 
 	/**
 	 * Almacena el nombre o título.
@@ -37,6 +53,56 @@ public class VistaListado extends VistaGeneral {
 	 * @see VistaGeneral#pedirConfirmación(String)
 	 */
 	public void mostrar(List<String> listaTextos) {
-		mostrarAviso("PENDIENTE: programador ocupado…");
+		int total = listaTextos.size();
+		int página = 0;
+		int inicio = 0;
+		int fin = TAMAÑO_PÁGINA;
+
+		while (inicio < total) {
+			if (fin > total) {
+				fin = total;
+			}
+			out.printf("Página %d de %d%n", ++página, (total / TAMAÑO_PÁGINA) + 1);
+			for (int i = inicio; i < fin; i++) {
+				out.printf(FORMATO_LISTADO, i + 1, listaTextos.get(i));
+			}
+			inicio += TAMAÑO_PÁGINA;
+			if (inicio < total) {
+				pedirConfirmación("Continuar porfavo...");
+			}
+		}
+	}
+	
+	/**
+	 * Método que gestiona el número de entradas, importaciones y exportaciones y borrados que 
+	 * será usado posteriormente en el método mostrarResumen.
+	 *  
+	 */
+	public void gestionarResumen() {
+		
+		int numEntradas = 0;
+		int numImportaciones = 0;
+		int numExportaciones = 0;
+		int numBorrados = 0;
+		
+		this.numEntradas = numEntradas;
+		this.numImportaciones = numImportaciones;
+		this.numExportaciones = numExportaciones;
+		this.numBorrados = numBorrados;
+	}
+	
+	/**
+	 * Incorporar un método asi como los atributos y los retoques necesarios para su gestión 
+	 * para mostrar un resumen del estado del inventario: número de entradas, informacion de importaciones
+	 * y exportaciones, borrados realizados, etc.
+	 */
+	
+	
+	public void mostrarResumen() {
+		out.println("Resumen del inventario:");
+		out.println("Número de entradas: " + numEntradas);
+		out.println("Número de importaciones: " + numImportaciones);
+		out.println("Número de exportaciones: " + numExportaciones);
+		out.println("Número de borrados realizados: " + numBorrados);
 	}
 }
